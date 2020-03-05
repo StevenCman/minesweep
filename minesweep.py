ff368082119c8ade89192a8ea68b839595835e95
Steven Crossman <scross198@outlook.com>
2020-03-04 20:30:31 -0500

    added leaderboard functionality

--- minesweep.py
+++ minesweep.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from tkinter import *
+import os
 import tkinter
 import webbrowser
 import random
@@ -20,6 +21,10 @@ def settings():
 		gameWindow.destroy()	
 	except:
 		print()
+	try:
+		wonGame.destroy()
+	except:
+		print()		
 
 	global settingsWindow
 	settingsWindow = tkinter.Tk()
@@ -52,6 +57,15 @@ def gameplay(size, mines, arr):
 	global gameWindow
 	global mineArray
 	global btnArray
+	global name
+	global na
+	global level
+	if (level == 1):
+		try:
+			name = na.get()
+		except:
+			print()
+	print(name)
 	try:
 		settingsWindow.destroy()
 	except:
@@ -123,7 +137,10 @@ def gameplay(size, mines, arr):
 		tutorial = Label(gameWindow, text="The mine is in the middle square, just click around it")
 		tutorial.pack(side=TOP)
 	elif(level == 2):
-		tutorial = label(gameWindow, text="The mines for this one are in a diagonal line (bottom left to top right)\n If you click the other 2 corners it will reveal the board")
+		tutorial = Label(gameWindow, text="The mines for this one are in a diagonal line (bottom left to top right)\n If you click the other 2 corners it will reveal the board")
+		tutorial.pack(side=TOP)
+	elif(level == 3):
+		tutorial = Label(gameWindow, text="No help for this one, its completely randomized except for the corners")	
 		tutorial.pack(side=TOP)
 	if(level == 0):
 		restartBtn = Button(gameWindow, text= "Restart", command=settings)
@@ -166,6 +183,10 @@ def quit():
 		lostGame.destroy()
 	except:
 		print()		
+	try:
+		lossWindow.destroy()
+	except:
+		print()	
 def mineMaker(mines, x, y, mineArray):
 	r = random.randint(0, x-1)
 	c = random.randint(0, y-1)
@@ -228,12 +249,18 @@ def hint(x1,y1,num):
 def left(event,t,u, num):
 	btnArray[t][u].unbind('<Button-1>')
 	btnArray[t][u].unbind('<Button-3>')
+	global lives
 
-	if(mineArray[t][u] == "*"):
+	if(mineArray[t][u] == "*" and level == 0):
 		lost()
+	elif(mineArray[t][u] == "*" and level > 0):
+		if(lives > 0):
+			lives -= 1
+			liveLoss()
+		else:
+			campaignLost()	
 	elif(mineArray[t][u] == 0):
 		openZeros(t, u)
-		time.sleep(2)
 	else:
 		btnArray[t][u].config(text=mineArray[t][u], state=DISABLED)
 	
@@ -267,6 +294,7 @@ def openZeros(x, y):
 			openZeros(x+1, y)		
 
 def winCheck(x, y, num):
+	global level
 	counter = 0
 	for t in range(len(btnArray)):
 		for u in range(len(btnArray[t])):
@@ -274,10 +302,9 @@ def winCheck(x, y, num):
 				counter += 1
 	print("Counter: " + str(counter))			
 	if (counter == num and level == 0):
-		time.sleep(3)
 		won()
 	elif(counter == num and level > 0):
-		time.sleep(3)
+		level += 1
 		campaign()
 
 def won():
@@ -286,7 +313,7 @@ def won():
 
 	wonGame = tkinter.Tk()
 	wonGame.title("CONGRATULATIONS!!")
-	wonGame.geometry("400x200")
+	wonGame.geometry("400x400")
 
 	victory = Image.open("C:/Python Crap/victory.jpg")
 	victory = victory.resize((100,100))
@@ -294,14 +321,19 @@ def won():
 	
 	over = Label(wonGame, text="You have succesfully located all mines and have saved the day")
 	over.pack(side=TOP)
+
 	restartBtn = Button(wonGame, text= "Restart", command= settings)
-	restartBtn.pack(side=TOP)
+	restartBtn.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 	vicLbl = Label(wonGame, image = victory)
 	vicLbl.image = victory
-	vicLbl.pack(side = TOP)
+	vicLbl.place(relx=0.5, rely=0.3, anchor=CENTER)
+
+	mainMen = Button(wonGame, text="Main Menu", command=mainMenu)
+	mainMen.place(relx=0.5, rely=0.7, anchor=CENTER)
+
 	quitBtn = Button(wonGame, text= "Quit", command=quit)
-	quitBtn.pack(side=BOTTOM)
+	quitBtn.place(relx=0.5, rely=0.9, anchor=CENTER)
 
 def lost():
 	gameWindow.destroy()
@@ -317,14 +349,26 @@ def lost():
 	over = Label(lostGame, text="You hit a mine and have lost the game")
 	over.pack(side=TOP)
 	restartBtn = Button(lostGame, text= "Restart", command= settings)
-	restartBtn.pack(side=TOP)
+	restartBtn.place(relx=0.5, rely=0.3, anchor=CENTER)
 
-	defLbl = Label(lostGame, image = defeat)
-	defLbl.image = defeat
-	defLbl.pack(side = TOP)
+	mainMen = Button(lostGame, text="Main Menu", command=mainMenu)
+	mainMen.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 	quitBtn = Button(lostGame, text= "Quit", command=quit)
-	quitBtn.pack(side=BOTTOM)
+	quitBtn.place(relx=0.5, rely=0.8, anchor=CENTER)
+
+def liveLoss():
+	gameWindow.destroy()
+	global liveWindow
+	liveWindow = tkinter.Tk()
+	liveWindow.title("Live Lost")
+	liveWindow.geometry("250x150")
+
+	li = Label(liveWindow, text="Unfortunately you triggered a mine\n Many innocents were killed\n But at least youre still alive to try again!")
+	li.pack(side=TOP)
+
+	con = Button(liveWindow, text="Continue on", command=campaign)
+	con.pack(side=BOTTOM)
 
 def campaign():
 	try:
@@ -334,68 +378,210 @@ def campaign():
 	try:
 		gameWindow.destroy()
 	except:
-		print()			
+		print()	
+	try:
+		liveWindow.destroy()
+	except:
+		print()				
 	global level
+	global lives
 	global campaignWindow
+	global na
+	na = 0
+	
 	campaignWindow = tkinter.Tk()
 	campaignWindow.title("Minesweep Level " + str(level))
 	campaignWindow.geometry("400x200")
 	x1 = 0
 	miner = 0
-	lives = 3
+	if(level == 0):
+		lives = 3
+		na = Entry(campaignWindow)
+		na.place(relx=0.5, rely=0.5, anchor=CENTER)
+		na.insert(0, "Name Here")
 	mineArr = 0
+
 	if (level == 0):
+		level += 1
+
+	if (level == 1):
 		Intro = Label(campaignWindow, text="Welcome to your Minesweep adventure!\n Hit continue below to get to your first minefield and start clearing land\n You can hit three mines before you are fired from your job")
 		Intro.pack(side=TOP)
 		mineArr = [[1,1,1], [1,"*",1], [1,1,1]]
 		x1=3
 		miner=1
-		level += 1
-		print (level)
-	elif(level == 1):
-		print("Here")
+		cont = Button(campaignWindow, text="Continue", command=lambda x=x1, mine=miner: gameplay(x,mine, mineArr))
+		cont.place(relx=0.5, rely=0.8, anchor=CENTER)
+	elif(level == 2):
 		Intro = Label(campaignWindow, text="Congratulations on clearing your first minefield!\n Its only going to get harder from here.")
 		Intro.pack(side = TOP)
 		mineArr = [["*",2,1,0,0],[2,"*",2,1,0],[1,2,"*",2,1],[0,1,2,"*",2],[0,0,1,2,"*"]]
 		x1=5
 		miner=5
-		level += 1
+		cont = Button(campaignWindow, text="Continue", command=lambda x=x1, mine=miner: gameplay(x,mine, mineArr))
+		cont.place(relx=0.5, rely=0.8, anchor=CENTER)
+	elif(level == 3):
+		Intro = Label(campaignWindow, text="This will be your first random minefield\n The corners will always be safe\n So if you dont have any moves just go to the corners")
+		Intro.pack(side = TOP) 
+		x1 = 5
+		miner = 5
+		cont = Button(campaignWindow, text="Continue", command=lambda x=x1, mine=miner: gameplay(x,mine, 0))
+		cont.place(relx=0.5, rely=0.8, anchor=CENTER)
+	elif(level > 3):
+		Intro = Label(campaignWindow, text="Good Job Out There!\n Another random minefield coming up\n Bigger than the last")
+		Intro.pack(side = TOP) 
+		x1 = level + 2
+		if (level < 7):
+			miner = ((level*level)/2)
+			miner = round(miner)
+		else:
+			miner = ((level*level)/2) + level	
+			miner = round(miner)
+
+		cont = Button(campaignWindow, text="Continue", command=lambda x=x1, mine=miner: gameplay(x,mine, 0))
+		cont.place(relx=0.5, rely=0.8, anchor=CENTER)
+
 	liveLbl = Label(campaignWindow, text="You have " + str(lives) +" lives left")
 	liveLbl.place(relx=0.5, rely=0.4, anchor=CENTER)
-	cont = Button(campaignWindow, text="Continue", command=lambda x=x1, mine=miner: gameplay(x,mine, mineArr))
-	cont.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-global flag
-global lives
-global level
-level = 0
-lives = 0
-root = tkinter.Tk()
-root.geometry("300x400")
-root.title('Shitty Minesweeper')
+def campaignLost():
+	gameWindow.destroy()
+	global lossWindow
+	global name
+	lossWindow = tkinter.Tk()
+	lossWindow.title("Defeat")
+	lossWindow.geometry("400x150")
 
-flag = Image.open("C:/Python Crap/minesweeperFlag.png")
-flag = flag.resize((50,50))
-flag = ImageTk.PhotoImage(flag)
+	print (name)
+	file = open("leaderboard.txt", "a")
+	file.write(str(name) + '\n')
+	file.write(str(level) + '\n')
+	file.close()
 
-title = Label(text="Shitty Minesweeper")
-title.pack(side = TOP)
+	defLabel = Label(lossWindow, text="This is the end of your career as a Minesweeper\n You have wasted everybodies time by\n failing to clear out the dangerous bombs\n Too many innocents to count are dead\n Shame on you")
+	defLabel.pack(side=TOP)
 
-label = Label(image=flag)
-label.image = flag
-label.pack(side = TOP)
+	manMenu= Button(lossWindow,text="Main Menu", command=mainMenu)
+	manMenu.pack(side=BOTTOM)
 
-campBtn = Button(root, text="Campaign", command=campaign)
-campBtn.place(relx=0.5, rely=0.3, anchor=CENTER)
+	qut = Button(lossWindow, text="Quit", command=quit)
+	qut.pack(side=BOTTOM)
 
-startBtn = Button(root, text="Quick Play", command=settings)
-startBtn.place(relx=0.5, rely=0.5, anchor=CENTER)
+def ldrBoard():
+	root.destroy()
+	global leaderWindow
+	leaderWindow = tkinter.Tk()
+	leaderWindow.title("Leaderboard")
+	leaderWindow.geometry("300x500")
 
-instructBtn = Button(root, text="Instructions", command=instructions)
-instructBtn.place(relx=0.5, rely=0.7, anchor=CENTER)
-
-quitBtn = Button(root, text="Quit", command=quit)
-quitBtn.place(relx=0.5, rely=0.85, anchor=CENTER)
+	if(os.path.exists("leaderboard.txt") == False):
+		file2 = open("leaderboard.txt", "w")
+		file2.close
 
 
-root.mainloop()
+	length = file_len("leaderboard.txt")
+	file = open("leaderboard.txt", "r")
+	lbl = Label(leaderWindow, text="Top Ten scores")
+	lbl.pack(side=TOP)
+	leaders = [0 for x in range(int(length/2))]
+	nameLbls = [0 for x in range(10)]
+	scoreLbls = [0 for x in range(10)]
+
+	frame = Frame(leaderWindow)
+	
+	for x in range(int(length/2)):
+		leaders[x] = (file.readline(), file.readline())
+	file.close()
+	leaders.sort(key=scoreSorter, reverse=True)
+
+	listSize = 0
+	if(length/2 > 10):
+		listSize = 10
+	else:
+		listSize = length/2
+
+	if (listSize==0):
+		noth = Label(frame, text="There are no entries yet")
+		noth.grid(row=0, column=0)
+	for x in range(int(listSize)):	
+		nameLbls[x] = Label(frame, text=str(leaders[x][0]))
+		scoreLbls[x] = Label(frame, text=str(leaders[x][1]))
+		nameLbls[x].grid(row=x, column=0)
+		scoreLbls[x].grid(row=x, column=5)
+
+
+	frame.pack(side=TOP)
+	backButton = Button(leaderWindow, text="Back", command=mainMenu)
+	backButton.pack(side=BOTTOM)
+
+def file_len(fname):
+	if os.stat(fname).st_size == 0:
+		return 0
+	with open(fname) as f:
+		for i, l in enumerate(f):
+			pass
+	return i + 1
+
+def scoreSorter(list):
+	return int(list[1])
+
+def mainMenu():
+	try:
+		leaderWindow.destroy()
+	except:
+		print()
+	try:
+		lostGame.destroy()
+	except:
+		print()
+	try:
+		campaignLost.destroy()
+	except:
+		print()
+	try:
+		wonGame.destroy()
+	except:
+		print()
+
+	global flag
+	global lives
+	global level
+	global name
+	global root
+	name = 0
+	level = 0
+	lives = 0
+	root = tkinter.Tk()
+	root.geometry("300x400")
+	root.title('Shitty Minesweeper')
+
+	flag = Image.open("C:/Python Crap/minesweeperFlag.png")
+	flag = flag.resize((50,50))
+	flag = ImageTk.PhotoImage(flag)
+
+	title = Label(text="Shitty Minesweeper")
+	title.pack(side = TOP)
+
+	label = Label(image=flag)
+	label.image = flag
+	label.pack(side = TOP)
+
+	campBtn = Button(root, text="Campaign", command=campaign)
+	campBtn.place(relx=0.5, rely=0.3, anchor=CENTER)
+
+	startBtn = Button(root, text="Quick Play", command=settings)
+	startBtn.place(relx=0.5, rely=0.475, anchor=CENTER)
+
+	instructBtn = Button(root, text="Instructions", command=instructions)
+	instructBtn.place(relx=0.5, rely=0.65, anchor=CENTER)
+
+	leaderboardBtn = Button(root, text="Leaderboard", command=ldrBoard)
+	leaderboardBtn.place(relx=0.5, rely=0.8, anchor=CENTER)
+
+	quitBtn = Button(root, text="Quit", command=quit)
+	quitBtn.place(relx=0.5, rely=0.95, anchor=CENTER)
+
+
+	root.mainloop()
+
+mainMenu()
